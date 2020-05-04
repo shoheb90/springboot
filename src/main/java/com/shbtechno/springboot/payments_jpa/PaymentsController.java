28b9fdfb90e8b57ac5f3900508e0e6c0b32a6196
shoheb <shbtechno@gmail.com>
2020-05-04 18:55:27 +0100

    First project commit

---
+++ src/main/java/com/shbtechno/springboot/payments_jpa/PaymentsController.java
@@ -0,0 +1,45 @@
+package com.shbtechno.springboot.payments_jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*
+   @Author: Shoheb 
+*/
+@RestController
+public class PaymentsController {
+
+    @Autowired
+    private PaymentServices paymentServices;
+
+    @RequestMapping("/payments")
+    public List<Payments> getAllPayments() {
+        return paymentServices.getAllPayments();
+    }
+
+    //GET REQUEST WITH PARAMETER
+    @RequestMapping("/payments/{paymentID}")
+    public Payments getPayments(@PathVariable String paymentID){
+        return paymentServices.getPayments(paymentID);
+    }
+
+    //POST REQUEST
+    @RequestMapping(method = RequestMethod.POST,value = "/payments")
+    public void addPayments(@RequestBody Payments payments){
+        paymentServices.addPayment(payments);
+    }
+
+    //PUT REQUEST
+    @RequestMapping(method = RequestMethod.PUT,value = "/payments/{paymentID}")
+    public void updatePayments(@RequestBody Payments payments, @PathVariable String paymentID){
+        paymentServices.updatePayment(payments,paymentID);
+    }
+
+    //DELETE REQUEST
+    @RequestMapping(method = RequestMethod.DELETE,value = "/payments/{paymentID}")
+    public void deletePayments(@PathVariable  String paymentID){
+        paymentServices.deletePayment(paymentID);
+    }
+}

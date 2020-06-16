8eeca6d1177d581242ee59f5d3a9778c758d8362
shoheb <shbtechno@gmail.com>
2020-06-16 16:27:34 +0100

    Sample commit for zaid

---
+++ src/main/java/com/shbtechno/springboot/Addition.java
@@ -0,0 +1,15 @@
+package com.shbtechno.springboot;
+/*
+   @Author      : Shoheb
+   Description  : 
+   >>>>>>>>>>>>>>>>>>>>> Annotations >>>>>>>>>>>>>>>>>>>>>  
+   @      :   
+   @      :   
+   
+ */
+
+public class Addition {
+    public void main (String[] args){
+        System.out.println("zaid");
+    }
+}
